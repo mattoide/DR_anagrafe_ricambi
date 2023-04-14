c63e8b77b6c711ed1b81131943e10b6431171154
Mattia <mattiabellotti.dev@gmail.com>
2023-04-14 12:33:55 +0200

    added button for restart

--- update/upload_for_update_page.php
+++ update/upload_for_update_page.php
@@ -7,10 +7,23 @@
     include "../load.php"
     ?>
 
+    <script>
+        function restart(){
+            window.location.href = "../select_action_page.php";
+        }
+
+    </script>
+
 </head>
 <body>
 
 <form class="centered" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data" >
+    <div>
+        <button style="background: gray"  type="button" value="Ricomincia" onclick="restart()">
+            <span class=reload>&#x21bb;</span>
+        </button>
+    </div>
+
     <h1 class="title">AGGIORNAMENTO</h1>
     <h2 class="title">Carica file</h2>
     <input  type="file" name="fileToUpload" id="fileToUpload" accept=".xls,.xlsx,.ods">
